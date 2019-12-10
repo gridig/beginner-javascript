bebd261f011fd5341dc385c2e42587226c177960
gridig <igorabdul@gmail.com>
2019-12-10 14:05:06 +0100

    module 5 done!

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,5 +1,6 @@
 const butts = document.querySelector('.butts');
 const coolButton = document.querySelector('.cool');
+const buyButtons = document.querySelectorAll('button.buy');
 
 function handleClick() {
   console.log('🐛 IT GOT CLICKED!!!');
@@ -10,22 +11,18 @@ const hooray = () => console.log('HOORAY!');
 butts.addEventListener('click', function() {
   console.log('Im an anon!');
 });
+
 coolButton.addEventListener('click', hooray);
 
 butts.removeEventListener('click', handleClick);
 
-// Listen on multiple items
-const buyButtons = document.querySelectorAll('button.buy');
-
-function handleBuyButtonClick(event) {
-  console.log('You clicked a button!');
-  const button = event.target;
-  // console.log(button.textContent);
-  // console.log(parseFloat(event.target.dataset.price));
-  console.log(event.target);
-  console.log(event.currentTarget);
-  console.log(event.target === event.currentTarget);
-  // Stop this event from bubbling up
+function handleBuyButtonClick(e) {
+  const button = e.target;
+  console.log('You clicked a button');
+  // console.log(parseFloat(button.dataset.price));
+  console.log(e.target);
+  console.log(e.currentTarget);
+  console.log(e.target === e.currentTarget);
   // event.stopPropagation();
 }
 
@@ -36,18 +33,28 @@ buyButtons.forEach(function(buyButton) {
 window.addEventListener(
   'click',
   function(event) {
-    console.log('YOU CLICKED THE WINDOW');
+    console.log('You clicked a window');
     console.log(event.target);
     console.log(event.type);
+    console.log(event.bubble);
     // event.stopPropagation();
-    console.log(event.bubbles);
   },
   { capture: true }
 );
 
+// function buyItem() {
+//   console.log('buying item');
+// }
+
+// function attachListener(buyButton) {
+//   buyButton.addEventListener('click', buyItem);
+// }
+
+// buyButtons.forEach(attachListener);
+
 const photoEl = document.querySelector('.photo');
 
-photoEl.addEventListener('mouseenter', e => {
+photoEl.addEventListener('mouseenter', function(e) {
   console.log(e.currentTarget);
-  console.log(this);
+  // console.log(this);
 });
---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,44 @@
+const wes = document.querySelector('.wes');
+const signupForm = document.querySelector('[name="signup"]');
+
+wes.addEventListener('click', function(event) {
+  const shouldChangePage = confirm(
+    'this website might be malicious, do you wish to procede?'
+  );
+  if (!shouldChangePage) {
+    event.preventDefault();
+  }
+});
+
+signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = event.currentTarget.name.value;
+  const email = event.currentTarget.email.value;
+  const agree = event.currentTarget.agree.checked;
+
+  if (name.includes('chad')) {
+    alert('sry bro');
+    event.preventDefault();
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
+signupForm.name.addEventListener('focus', logEvent);
+signupForm.name.addEventListener('blur', logEvent);
+
+const photo = document.querySelector('.photo');
+
+function handlePhotoClick(event) {
+  if (event.type === 'click' || event.key === 'Enter') {
+    console.log('you clicked a photo');
+  }
+}
+
+photo.addEventListener('click', handlePhotoClick);
+photo.addEventListener('keyup', handlePhotoClick);
