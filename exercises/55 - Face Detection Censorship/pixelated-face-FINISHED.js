bff2ae7da98113862ba6a569dac569850bba0067
gridig <igorabdul@gmail.com>
2020-02-04 10:43:15 +0100

    fdc done!

--- exercises/55 - Face Detection Censorship/pixelated-face-FINISHED.js
+++ exercises/55 - Face Detection Censorship/pixelated-face-FINISHED.js
@@ -1,13 +1,9 @@
 const video = document.querySelector('.webcam');
-
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
-
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
-
-const faceDetector = new FaceDetector();
-
+const faceDetector = new window.FaceDetector();
 const optionsInputs = document.querySelectorAll(
   '.controls input[type="range"]'
 );
@@ -21,16 +17,17 @@ function handleOption(event) {
   const { value, name } = event.currentTarget;
   options[name] = parseFloat(value);
 }
-
 optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
+// Write a fucntion that will populate the users video
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: { width: 1280, height: 720 },
   });
   video.srcObject = stream;
   await video.play();
-  // canvases same size as video
+  // size the canvases to be the same size as the video
+  console.log(video.videoWidth, video.videoHeight);
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   faceCanvas.width = video.videoWidth;
@@ -39,6 +36,7 @@ async function populateVideo() {
 
 async function detect() {
   const faces = await faceDetector.detect(video);
+  // ask the browser when the next animation frame is, and tell it to run detect for us
   faces.forEach(drawFace);
   faces.forEach(censor);
   requestAnimationFrame(detect);
@@ -47,37 +45,41 @@ async function detect() {
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = '#fe8300';
+  ctx.strokeStyle = '#ffc600';
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
 }
 
 function censor({ boundingBox: face }) {
   faceCtx.imageSmoothingEnabled = false;
-  faceCtx.clearRect(0, 0, canvas.width, canvas.height);
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  // draw the small face
   faceCtx.drawImage(
-    video,
-    face.x,
+    // 5 source args
+    video, // where does the source come from?
+    face.x, // where do we start the source pull from?
     face.y,
     face.width,
     face.height,
-    face.x,
+    // 4 draw args
+    face.x, // where should we start drawing the x and y?
     face.y,
     options.SIZE,
     options.SIZE
   );
+  // draw the small face back on, but scale up
 
   const width = face.width * options.SCALE;
   const height = face.height * options.SCALE;
-
   faceCtx.drawImage(
-    faceCanvas,
-    face.x,
+    faceCanvas, // source
+    face.x, // where do we start the source pull from?
     face.y,
     options.SIZE,
     options.SIZE,
+    // Drawing args
     face.x - (width - face.width) / 2,
-    face.y - (width - face.heigth) / 2,
+    face.y - (height - face.height) / 2,
     width,
     height
   );
